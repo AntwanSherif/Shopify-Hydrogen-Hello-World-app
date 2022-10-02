6d71e3b8b27ffcd22a669d3b261b468103b08422
AntwanSherif <antwansherif@gmail.com>
2022-10-02 22:37:52 +0200

    :sparkles: Query products and variants

--- src/routes/collections/[handle].server.jsx
+++ src/routes/collections/[handle].server.jsx
@@ -8,6 +8,7 @@ import {
   ShopifyAnalyticsConstants
 } from '@shopify/hydrogen';
 import { Layout } from '../../components/Layout.server';
+import { ProductCard } from '../../components/ProductCard.server';
 
 export default function Collection() {
   const { handle } = useRouteParams();
@@ -44,12 +45,18 @@ export default function Collection() {
           </div>
         )}
       </header>
+
+      <section className='w-full gap-4 md:gap-8 grid p-6 md:p-8 lg:p-12'>
+        <div className='grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+          {collection.products.nodes.map(product => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </section>
     </Layout>
   );
 }
 
-// The `Seo` component uses the collection's `seo` values, if specified. If not
-// specified, then the component falls back to using the collection's `title` and `description`.
 const QUERY = gql`
   query CollectionDetails($handle: String!) {
     collection(handle: $handle) {
@@ -59,6 +66,40 @@ const QUERY = gql`
       seo {
         description
         title
+      }
+      image {
+        id
+        url
+        width
+        height
+        altText
+      }
+      products(first: 8) {
+        nodes {
+          id
+          title
+          publishedAt
+          handle
+          variants(first: 1) {
+            nodes {
+              id
+              image {
+                url
+                altText
+                width
+                height
+              }
+              priceV2 {
+                amount
+                currencyCode
+              }
+              compareAtPriceV2 {
+                amount
+                currencyCode
+              }
+            }
+          }
+        }
       }
     }
   }
