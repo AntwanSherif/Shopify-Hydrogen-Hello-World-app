ea347db9cb1a01b370cae9dd517659da71977a25
AntwanSherif <antwansherif@gmail.com>
2022-10-02 18:33:55 +0200

    :lipstick: Apply some styles

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <section className='p-6 md:p-8 lg:p-12'>
+      <h1 className='font-extrabold mb-4 text-5xl md:text-7xl'>Hello world!</h1>
+      <p className='font-bold mb-3'>Welcome to Hydrogen.</p>
+      <p>Hydrogen is a front-end web development framework used for building Shopify custom storefronts.</p>
+    </section>
+  );
 }
+
