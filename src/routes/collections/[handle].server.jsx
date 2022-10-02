bacd22abe5cc409dc0a722e4ac859e4e4afee505
AntwanSherif <antwansherif@gmail.com>
2022-10-02 22:28:28 +0200

    :sparkles: Query collection by handle

--- src/routes/collections/[handle].server.jsx
+++ src/routes/collections/[handle].server.jsx
@@ -1,14 +1,32 @@
-import { useRouteParams } from '@shopify/hydrogen';
+import { gql, useShopQuery, useRouteParams } from '@shopify/hydrogen';
 import { Layout } from '../../components/Layout.server';
 
 export default function Collection() {
   const { handle } = useRouteParams();
+
+  const {
+    data: { collection }
+  } = useShopQuery({
+    query: QUERY,
+    variables: {
+      handle
+    }
+  });
+
   return (
     <Layout>
       <section className='p-6 md:p-8 lg:p-12'>
-        This will be the collection page for <strong>{handle}</strong>
+        This will be the collection page for <strong>{collection.title}</strong>
       </section>
     </Layout>
   );
 }
+
+const QUERY = gql`
+  query CollectionDetails($handle: String!) {
+    collection(handle: $handle) {
+      title
+    }
+  }
+`;
 
