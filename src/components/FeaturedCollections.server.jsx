4485ee31148c13d270285c327c8d4fc738413580
AntwanSherif <antwansherif@gmail.com>
2022-10-02 18:55:25 +0200

    :sparkles: Display collections images

--- src/components/FeaturedCollections.server.jsx
+++ src/components/FeaturedCollections.server.jsx
@@ -1,4 +1,4 @@
-import { Link, gql, useShopQuery, CacheLong } from '@shopify/hydrogen';
+import { Link, Image, gql, useShopQuery, CacheLong } from '@shopify/hydrogen';
 
 export default function FeaturedCollections() {
   const {
@@ -16,6 +16,15 @@ export default function FeaturedCollections() {
           return (
             <Link key={collection.id} to={`/collections/${collection.handle}`}>
               <div className='grid gap-4'>
+                {collection?.image && (
+                  <Image
+                    className='rounded shadow-border overflow-clip inline-block aspect-[5/4] md:aspect-[3/2] object-cover'
+                    width={'100%'}
+                    height={336}
+                    alt={`Image of ${collection.title}`}
+                    data={collection.image}
+                  />
+                )}
                 <h2 className='whitespace-pre-wrap max-w-prose font-medium text-copy'>{collection.title}</h2>
               </div>
             </Link>
@@ -32,6 +41,12 @@ const QUERY = gql`
         id
         title
         handle
+        image {
+          altText
+          width
+          height
+          url
+        }
       }
     }
   }
