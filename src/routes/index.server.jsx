b7550ec31e8a0e5c295591e6f80f62a56d982e3d
AntwanSherif <antwansherif@gmail.com>
2022-10-02 18:53:57 +0200

    :sparkles: Show featured collections on Home page

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -1,13 +1,10 @@
 import { Layout } from '../components/Layout.server';
+import FeaturedCollections from '../components/FeaturedCollections.server';
 
 export default function Home() {
   return (
     <Layout>
-      <section className='p-6 md:p-8 lg:p-12'>
-        <h1 className='font-extrabold mb-4 text-5xl md:text-7xl'>Hello world!</h1>
-        <p className='font-bold mb-3'>Welcome to Hydrogen.</p>
-        <p>Hydrogen is a front-end web development framework used for building Shopify custom storefronts.</p>
-      </section>
+      <FeaturedCollections />
     </Layout>
   );
 }
