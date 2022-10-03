ae58606cc032f7b33d3b1c063371b368613bb566
AntwanSherif <antwansherif@gmail.com>
2022-10-03 12:07:31 +0200

    :recycle: Create header component

--- src/components/Layout.server.jsx
+++ src/components/Layout.server.jsx
@@ -1,13 +1,11 @@
 import { Suspense } from 'react';
 import { useShopQuery, CacheLong, gql, useUrl, Link, Seo } from '@shopify/hydrogen';
+import { Header } from './Header.client';
 
 /**
  * A server component that defines a structure and organization of a page that can be used in different parts of the Hydrogen app
  */
 export function Layout({ children }) {
-  const { pathname } = useUrl();
-  const isHome = pathname === '/';
-
   const {
     data: { shop }
   } = useShopQuery({
@@ -34,18 +32,7 @@ export function Layout({ children }) {
             Skip to content
           </a>
         </div>
-        <header
-          role='banner'
-          className={`flex items-center h-16 p-6 md:p-8 lg:p-12 sticky backdrop-blur-lg z-40 top-0 justify-between w-full leading-none gap-4 antialiased transition shadow-sm ${
-            isHome ? 'bg-black/80 text-white' : 'bg-white/80'
-          }`}
-        >
-          <div className='flex gap-12'>
-            <Link className='font-bold' to='/'>
-              {shop.name}
-            </Link>
-          </div>
-        </header>
+        <Header shop={shop} />
 
         <main role='main' id='mainContent' className='flex-grow'>
           <Suspense>{children}</Suspense>
