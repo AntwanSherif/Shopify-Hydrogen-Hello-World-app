e802403404ecfec71401cb48dcbc653ef67f582c
AntwanSherif <antwansherif@gmail.com>
2022-10-03 11:32:31 +0200

    :sparkles: Query product by handle

--- src/routes/products/[handle].server.jsx
+++ src/routes/products/[handle].server.jsx
@@ -1,15 +1,33 @@
-import { useRouteParams } from '@shopify/hydrogen';
+import { useRouteParams, gql, useShopQuery } from '@shopify/hydrogen';
 import { Layout } from '../../components/Layout.server';
 
 export default function Product() {
   const { handle } = useRouteParams();
 
+  const {
+    data: { product }
+  } = useShopQuery({
+    query: PRODUCT_QUERY,
+    variables: {
+      handle
+    }
+  });
+
   return (
     <Layout>
       <section className='p-6 md:p-8 lg:p-12'>
-        This will be the product page for <strong>{handle}</strong>
+        This will be the product page for <strong>{product.title}</strong>
       </section>
     </Layout>
   );
 }
+
+const PRODUCT_QUERY = gql`
+  query Product($language: LanguageCode, $handle: String!) @inContext(language: $language) {
+    product(handle: $handle) {
+      id
+      title
+    }
+  }
+`;
 
