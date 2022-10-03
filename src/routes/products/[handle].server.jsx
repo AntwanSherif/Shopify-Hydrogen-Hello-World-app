029a746a1c32bce0b0a38bace8d3b0149173ae47
AntwanSherif <antwansherif@gmail.com>
2022-10-03 11:43:48 +0200

    :card_file_box: Query product and variants details

--- src/routes/products/[handle].server.jsx
+++ src/routes/products/[handle].server.jsx
@@ -41,15 +41,85 @@ export default function Product() {
 }
 
 const PRODUCT_QUERY = gql`
-  query Product($language: LanguageCode, $handle: String!) @inContext(language: $language) {
+  fragment MediaFields on Media {
+    mediaContentType
+    alt
+    previewImage {
+      url
+    }
+    ... on MediaImage {
+      id
+      image {
+        url
+        width
+        height
+      }
+    }
+    ... on Video {
+      id
+      sources {
+        mimeType
+        url
+      }
+    }
+    ... on Model3d {
+      id
+      sources {
+        mimeType
+        url
+      }
+    }
+    ... on ExternalVideo {
+      id
+      embedUrl
+      host
+    }
+  }
+  query Product($handle: String!) {
     product(handle: $handle) {
       id
       title
       vendor
       descriptionHtml
+      media(first: 7) {
+        nodes {
+          ...MediaFields
+        }
+      }
+      variants(first: 100) {
+        nodes {
+          id
+          availableForSale
+          compareAtPriceV2 {
+            amount
+            currencyCode
+          }
+          selectedOptions {
+            name
+            value
+          }
+          image {
+            id
+            url
+            altText
+            width
+            height
+          }
+          priceV2 {
+            amount
+            currencyCode
+          }
+          sku
+          title
+          unitPrice {
+            amount
+            currencyCode
+          }
+        }
+      }
       seo {
-        title
         description
+        title
       }
     }
   }
